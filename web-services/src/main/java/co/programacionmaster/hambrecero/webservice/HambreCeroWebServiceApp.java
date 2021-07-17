50f8dfb9a3e226ed0e0aafaab090f2ac9bfd0aba
barcochrist <barcochrist@gmail.com>
2021-07-17 18:16:34 -0500

    chore: create modular project structure

---
+++ web-services/src/main/java/co/programacionmaster/hambrecero/webservice/HambreCeroWebServiceApp.java
@@ -0,0 +1,12 @@
+package co.programacionmaster.hambrecero.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication()
+public class HambreCeroWebServiceApp {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HambreCeroWebServiceApp.class, args);
+  }
+}
