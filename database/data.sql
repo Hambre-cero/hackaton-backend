aae895370b30f0a47d98f757f04e1ea93f29922b
barcochrist <barcochrist@gmail.com>
2021-07-23 13:04:05 -0500

    chore: add database inserts script file

---
+++ database/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (id, "name", email, "password", created_on, created_by, status)
+VALUES('58dc8a98-8c0e-4785-aff8-3d10f457a58f', 'Christian Barco', 'barcochrist@gmail.com', '$2a$10$Sei/X9uRZ0037R3g/wxYa.yC6iKKn91h4gpnTJJTa1PvLr7wYgS2m', '2021-07-22 12:24:54.000', '58dc8a98-8c0e-4785-aff8-3d10f457a58f', 'ACTIVE');
+
+INSERT INTO organizations
+(id, identification, "name", country, city, province, address, postal_code, status, created_on, created_by, phone_number, phone_number_2)
+VALUES('58dc8a98-8c0e-4785-aff8-3d10f457a78a', '800 176112-4', 'Fundación Paz y Bien', 'CO', 'Cali', 'Valle del Cauca', 'Diagonal 26 I2 No. 80A-25', '5295', 'ACTIVE', '2021-07-22 12:24:54.000', '58dc8a98-8c0e-4785-aff8-3d10f457a58f', '(57) 2-4039640', NULL);
